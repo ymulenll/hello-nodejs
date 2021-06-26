d426e9950e21adc7b21c33651942ed83c3fd5e5a
Yoelvis Mulen <ymulenll@gmail.com>
2021-06-26 20:31:20 -0300

    update message

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running at ${port}/`);
+  console.log(`Server running on port: ${port}`);
 });
